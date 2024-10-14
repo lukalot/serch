0b242441f702b9f0f32943f07efc0c2cdc6cd06d
Luke N. Arnold <lukenarnold@gmail.com>
2024-10-14 16:50:01 -0700

    also fix spaces for regex match

--- rerouter.js
+++ rerouter.js
@@ -1,16 +1,16 @@
 // Array of search engine configurations
 const searchEngines = [
-    { regex: /^p\+(.+)/i, destination: 'https://www.perplexity.ai/search?q=' },
-    { regex: /^ddg\+(.+)/i, destination: 'https://duckduckgo.com/?q=' },
-    { regex: /^w\+(.+)/i, destination: 'https://en.wikipedia.org/wiki/Special:Search?search=' },
-    { regex: /^(at what|how to|how does|what does|when does|where does|if the|when the|look up|find the|search for|what is|where is|why does)/i, destination: 'https://www.perplexity.ai/search?q=' },
+    { regex: /^p\s+(.+)/i, destination: 'https://www.perplexity.ai/search?q=' },
+    { regex: /^ddg\s+(.+)/i, destination: 'https://duckduckgo.com/?q=' },
+    { regex: /^w\s+(.+)/i, destination: 'https://en.wikipedia.org/wiki/Special:Search?search=' },
+    { regex: /^(at what|how to|how does|what does|when does|where does|if the|when the|look up|find the|info on|search for|what is|where is|why does|tell me|write about|explain)\s+(.+)/i, destination: 'https://www.perplexity.ai/search?q=' },
     { regex: /^(.+)/i, destination: 'https://duckduckgo.com/?q=' }, // Catch-all for DuckDuckGo
 ];
 
 // Function to extract search query from URL
 function getSearchQueryFromUrl() {
     const path = window.location.pathname.substring(1); // Remove leading slash
-    return path ? decodeURIComponent(path) : null;
+    return path ? decodeURIComponent(path.replace(/\+/g, ' ')) : null;
 }
 
 // Function to perform rerouting based on search query
